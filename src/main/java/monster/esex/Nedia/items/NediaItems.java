ae6d7ce6acf226c030d8c8f0891791c86b826c28
aaron <aarongrahl98@gmail.com>
2024-11-27 01:34:54 -0500

    commit initial

--- src/main/java/monster/esex/Nedia/items/NediaItems.java
+++ src/main/java/monster/esex/Nedia/items/NediaItems.java
@@ -4,10 +4,13 @@ import monster.esex.Nedia.Nedia;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class NediaItems {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Nedia.MODID);
 
+    public static final RegistryObject<Item> ITEM1 = ITEMS.register("item1",
+            () -> new Item(new Item.Properties().stacksTo(64)));
 
 }
